e3634c4ce9e2da5632337d39ce425656b7c2989e
maximKaschitskiy <sipus2006@yandex.ru>
2022-12-29 23:30:24 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import mock from "../mock/page";
+
+const pasreHTMLString = (string) => { // translate layout string to DOM-tree
+  const parser = new DOMParser();
+  return parser.parseFromString(string, "text/html");
+};
+
+const getNodes = (string) => { // get entire content from <body>-tag
+  const doc = pasreHTMLString(string);
+  return doc.querySelector("body");
+};
+
+const parseNodes = (string) => { // get all text nodes
+  const strings = [];
+  const walking = (string) => {
+    string.childNodes.forEach((element) => {
+      if (
+        element.nodeName === "#text" &&
+        element.nodeType == 3 &&
+        !/SCRIPT|STYLE/.test(element.parentNode.tagName)
+      ) {
+        strings.push(element.textContent);
+        return;
+      } else {
+        walking(element);
+      }
+    });
+  };
+  walking(getNodes(string));
+  return strings;
+};
+
+const parseStrings = (string) =>  // clean-up unnecessary symbols
+  parseNodes(string).map(element =>
+    element
+      .replace(/[^A-Za-zА-Яа-я\s]/gi, " ")
+      .trim()
+      .replace(/\s+/g, " ")
+  );
+
+const parseWords = (string) => // filter for leave only unique and long words
+  new Set( 
+      parseStrings(string)
+        .map((element) => element.split(" "))
+        .flatMap((element) => element)
+        .filter((element) => element.length > 3)
+        .map((element) => element.toLowerCase()),
+  );
+
+const appendAnswer = (string) => {  // add answer in document
+  const data = parseWords(string);
+  const container = document.querySelector(".content");
+  const template = document.querySelector(".template");
+  const templateConteiner = template.content.querySelector(".template__conteiner");
+  const allWords = template.content.querySelector(".all-words");
+  const wordsCount = template.content.querySelector(".words-count");
+
+  allWords.childNodes[2].textContent = [...data].toString();
+  wordsCount.childNodes[1].textContent = `Ответ: ${[...data].length} уникальных слов`;
+
+  container.appendChild(templateConteiner);
+};
+
+window.addEventListener("load", () => {  // run script when content load
+  appendAnswer(mock);
+});
